000ab1b3889ace886d96e164fb2b55976c5e0c06
aalok.sathe <aalok.sathe@gmail.com>
2018-12-20 00:12:42 -0500

    remove stray instantiation in script; add docstring to constructor; make some things PEP-compliant style

--- epgen.py
+++ epgen.py
@@ -2,16 +2,21 @@ import random # to randomize the episode
 import tvdb_api # to retrieve the name and the number of episodes of a show
 import sys # to log error to stderr
 
-shows = tvdb_api.Tvdb()
-
 
 class EpisodeRandomizer:
     def __init__(self, showname=None, season=None, episode=None):
-        if season is not None or episode is not None:
-            raise NotImplementedError
+        """
+        Contructor
+        ---
+        showname -> str: the approximate name of the show
+        season -> int: the specific season to limit to (default: None)
+        episode -> int: a specific episode (from all seasons unless specified)
+                        to limit randomization to (default: None)
+        """
+        self.season, self.episode = season, episode
         self.tvdb = tvdb_api.Tvdb()
         try:
-            print("INFO: looking for show information", file=sys.stderr)
+            # print("INFO: looking for show information", file=sys.stderr)
             self.show = self.tvdb[showname]
         except tvdb_api.tvdb_shownotfound as error:
             print("ERR: no such TV show: {}".format(showname), file=sys.stderr)
@@ -33,30 +38,34 @@ class EpisodeRandomizer:
     def get_season(self, exclude=None):
         if exclude is not None:
             raise NotImplementedError
-        if not hasattr(self, 'numseasons'):
+        if self.season is not None: # user requested a particular season
+            return self.season
+        if not hasattr(self, 'numseasons'): # run only once
             self.numseasons = self.get_numseasons()
         return random.randint(1, self.numseasons)
 
     def get_episode(self, season, exclude=None):
         if exclude is not None:
             raise NotImplementedError
+        if self.episode is not None: # user requested a particular episode num
+            return self.episode
         return random.randint(1, self.get_numepisodes(season))
 
-    def get_random_ep(self):
+    def get_random_ep(self): # the star method of the class
         season = self.get_season()
         episode = self.get_episode(season)
         return season, episode
 
-    def print_random_episode(self):
+    def print_random_episode(self): # pretty-print the output of the randomizer
         result = self.get_random_ep()
         # print("The Random Generator says:")
-        print("Season {} Episode {} of {}".format(*result,
-                                                  self.show['seriesName']))
-        return result
-        
-if __name__ == '__main__':
+        print("Season {:d} Episode {:d} of {}".format(*result,
+                                                      self.show['seriesName']))
+        return result # also return it, just in case it's needed for sth else
+
+if __name__ == '__main__': # run the print method only if we're main
     if len(sys.argv) == 1:
         print("Usage: python epgen.py [name_of_show]")
         raise SystemExit
     randomizer = EpisodeRandomizer(" ".join(sys.argv[1:]))
-    randomizer.print_random_episode()
+    random = randomizer.print_random_episode()
