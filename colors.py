b47fe06c0a51f3d98445c6451e32abc31c6d70d9
aalok.sathe <aalok.sathe@gmail.com>
2018-12-22 01:06:34 -0600

    add a small info comment

--- colors.py
+++ colors.py
@@ -1,7 +1,12 @@
-RED   = "\033[1;31m"  
-BLUE  = "\033[1;34m"
-CYAN  = "\033[1;36m"
+"""
+This file houses POSIX codes to format text in a terminal.
+This file and its contents is to be treated as a Python source file, and as
+though it were housing an enum.
+"""
+RED = "\033[1;31m"
+BLUE = "\033[1;34m"
+CYAN = "\033[1;36m"
 GREEN = "\033[0;32m"
 RESET = "\033[0;0m"
-BOLD    = "\033[;1m"
+BOLD = "\033[;1m"
 REVERSE = "\033[;7m"
